f5d88f080e1350ae6d37d654cdcbc0bb32cfa116
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2025-01-10 14:24:14 -0300

    Update somavetorMPI.cpp

--- MPI/somavetorMPI.cpp
+++ MPI/somavetorMPI.cpp
@@ -1,9 +1,9 @@
+#include <mpi.h>
 #include <iostream>
-#include <omp.h>
- 
+
 int main(int argc, char** argv) {
 
-     MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -14,35 +14,38 @@ int main(int argc, char** argv) {
 
     int elementos_por_processos = tam / size;
     int resto = tam % size;
-    
+
     int start_index = rank * elementos_por_processos;
     int end_index = start_index + elementos_por_processos;
-    if(rank == size - 1) {
-        end_index+=resto;
+    if (rank == size - 1) {
+        end_index += resto;
     }
 
-    for(int i = start_index; i < end_index; i++) {
+    for (int i = start_index; i < end_index; i++) {
         soma_local += vetor[i];
     }
-    
+
     int soma_total = 0;
-    
-    if(rank != 0) {
+
+    if (rank != 0) {
         MPI_Send(&soma_local, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
-
+    
     if(rank == 0) {
-
-        MPI_Recv(&soma_total, 1, MPI_INT, 0, )
+        soma_total = soma_local;
+        for (int i = 1; i < size; i++) {
+            int soma_recebida;
+            MPI_Recv(&soma_recebida, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            soma_total += soma_recebida;
+        }
     }
 
-    printf("\nSoma do processo %d = %d\n", rank, soma_local);
+    printf("\nProcesso %d, soma local = %d\n", rank, soma_local);
 
-    if(rank == 0) {
-        printf("\nSoma: %d\n", soma_total);
+    if (rank == 0) {
+        printf("\nSoma total: %d\n", soma_total);
     }
 
     MPI_Finalize();
-
     return 0;
 }
