375aa26dd55bc647630148b2c9250c41cee622c7
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2025-01-16 14:53:01 -0300

    Add files via upload

--- MPI/pipeline.cpp
+++ MPI/pipeline.cpp
@@ -2,6 +2,7 @@
 #include <random>
 
 #include <mpi.h>
+#include <omp.h>
 
 int main(int argc, char **argv) {
 
@@ -9,53 +10,78 @@ int main(int argc, char **argv) {
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
     const int tam = 100000;
-    int vetor1[tam];
-    int vetor2[tam];
+    int vetor1[tam] = {0};
+    int vetor2[tam] = {0};
+    int vetor3[tam] = {0};
+    int soma = 0;
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<>dis(1, 100);
 
-    for(int i = 0; i < tam; i++) {
-        vetor1[i] = dis(gen);
-    }
+   
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 
     MPI_Init(&argc, &argv);
-    int rank, size;
+    int rank;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int elementos_por_processo = 25000;
 
     if(rank == 0) {
 
-        for(int i = 0; i < tam; i+=elementos_por_processo){
-            for(int j = 0; j < elementos_por_processo; j++) {
-                vetor1[i + j] *= 2;
-            }
-            MPI_Send(&vetor1[i], elementos_por_processo, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        for(int i = 0; i < tam; i++) {
+            vetor1[i] = i+1;
+        }
+
+        for(int i = 0; i < tam; i+=elementos_por_processo) {
+            MPI_Send(&vetor1[i], elementos_por_processo, MPI_INT, 1, 99, MPI_COMM_WORLD);
+        }
+    }
+
+    if(rank == 0) {
+        printf("\n\n");
+        for(int i = 0; i < 10; i++) {
+            std::cout << vetor1[i] << ", ";
         }
     }
 
     if(rank == 1) {
         
         for(int i = 0; i < tam; i += elementos_por_processo) {
-            MPI_Recv(&vetor2[i], elementos_por_processo, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&vetor2[i], elementos_por_processo, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+            for(int j = 0; j < elementos_por_processo; j++) {
+                vetor2[i + j] *= vetor2[i + j];
+            }
+                MPI_Send(&vetor2[i], elementos_por_processo, MPI_INT, 2, 100, MPI_COMM_WORLD);
+            }
         }
+
+    if(rank == 2) {
+        
+        for(int i = 0; i < tam; i += elementos_por_processo) {
+            MPI_Recv(&vetor3[i], elementos_por_processo, MPI_INT, 1, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        #pragma omp parallel for reduction(+:soma)
+        for(int i = 0; i < tam; i++) {
+            soma += vetor3[i];
+        }
+        std::cout << "\n\nSoma total: " << soma << std::endl;
     }
 
     
     if(rank == 0) {
-        std::cout << "Primeiros 100 elementos do vetor 1 " << std::endl;
-        for(int i = 0; i < 100; i++) {
+        std::cout << "\n\nPrimeiros 10 elementos do vetor 1 " << std::endl;
+        for(int i = 0; i < 10; i++) {
             std::cout << vetor1[i] << ", ";
         }
-
-        std::cout << "\n\n\nPrimeiros 100 elementos do vetor 2 " << std::endl;
-        for(int i = 0; i < 100; i++) {
+    } else if(rank == 1){
+        std::cout << "\n\n\nPrimeiros 10 elementos do vetor 2 " << std::endl;
+        for(int i = 0; i < 10; i++) {
             std::cout << vetor2[i] << ", ";
         }
     }
