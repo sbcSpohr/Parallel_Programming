960b4dbb8a42bec14e8b80638f6c708628a22965
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2025-02-10 11:06:14 -0300

    Update wordcountMPI.cpp

--- MPI/wordcountMPI.cpp
+++ MPI/wordcountMPI.cpp
@@ -3,53 +3,66 @@
 #include <unordered_map>
 #include <vector>
 #include <fstream>
-
+#include <sstream>
 #include <mpi.h>
 
 using namespace std;
-
-unordered_map<string, int> count(string &texto) {
+ 
+unordered_map<string, int> countWords(string &texto) {
 
     unordered_map<string, int> word_count;
     string word;
-    stringstream texto(texto);
+    stringstream text(texto);
 
-    while(getline(texto, word, ' ')) { //le do arquivo texto e armazena em word delimitando por ' '
+    while(getline(text, word, ' ')) {
         word_count[word]++;
     }
     return word_count;
 }
 
-int main(int argc, char **argv[]) {
-
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
-    int rank, size_mpi;
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size_mpi);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    vector<string> lines;
-    int total_lines = 0;
-
+    string full_text;
+    int total_size = 0;
+    
     if(rank == 0) {
         ifstream file(argv[1]);
         if(!file) {
-            cout << "ERRO" << endl;
+            cout << "ERRO arquivo" << endl;
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
 
-        string line;
-        while(getline(file, line)) { //le uma linha do arquivo file e armazena em line
-            lines.push_back(line);
-        }
-        file.close()
-        total_lines = lines.size();
-            
-        }
-
-        MPI_Bcast(&total_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-        int process_lines = (rank < total_lines % size_mpi) ? (total_lines / size_mpi + 1) : (total_lines / size_mpi);
-
-
+        stringstream store_text;
+        store_text << file.rdbuf();     //armazena todo conteudo do arquivo na ss store_text (usar uma ss para facilitar o fluxo de dados com operador "<<")
+        full_text = store_text.str();   //converte para uma string o store_text e armazena em full_text
+        file.close();
+        total_size = full_text.size();
     }
+
+    MPI_Bcast(&total_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    full_text.resize(total_size);
+    MPI_Bcast(&full_text[0], total_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    int start = (total_size  / size) * rank;
+    int end = (rank == size - 1) ? total_size : (total_size / size) * (rank + 1);
+
+    while(start > 0 && &full_text[start] != ' ') {
+        start++;
+    }
+    while(end < total_size  && &full_text[end] != ' ') {
+        end++;
+    }
+
+    
+
+
+
+
+
+}
