782f9670782fb15c401e6dee20e41047bb07cd10
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2025-03-07 16:00:47 -0300

    Add files via upload

--- MPI/wordcount/wordcount_mapreduce/wordcount_mapreduce2.cpp
+++ MPI/wordcount/wordcount_mapreduce/wordcount_mapreduce2.cpp
@@ -23,7 +23,6 @@ vector<pair<string, int>> count_words(string &text) {
     return count;
 }
 
-
 int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
@@ -48,11 +47,7 @@ int main(int argc, char **argv) {
             int chunk_size = word.size();
             MPI_Send(&chunk_size, 1, MPI_INT, index, 0, MPI_COMM_WORLD);
             MPI_Send(word.data(), word.size(), MPI_CHAR, index, 0, MPI_COMM_WORLD);
-
-            index++;
-            if(index == size - 1) {
-                index = 2;
-            }
+            index = (index == size - 1) ? 2 : index + 1;    
         }
 
         for(int i = 2; i < size; i++) {
@@ -79,25 +74,24 @@ int main(int argc, char **argv) {
             string phrase_text(word_recv.begin(), word_recv.end());
 
             local_count = count_words(phrase_text);
-            
+        }
+
             for(auto &word : local_count) { 
 
                 int word_size = word.first.size();
                 MPI_Send(&word_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-                MPI_Send(word.first.data(), word.first.size(), MPI_CHAR, 0, 0, MPI_COMM_WORLD);  
+                MPI_Send(word.first.data(), word.first.size(), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+                
             }
-        }
 
             int stop_signal = -1;
             MPI_Send(&stop_signal, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
-    if(rank == 0) {
-        
-        int workers_finish = 1;
+    if (rank == 0) {
+        int workers_finish = 0;
     
         while (workers_finish < size - 1) {
-
             MPI_Status status;
             int recv_size;
     
@@ -106,46 +100,13 @@ int main(int argc, char **argv) {
             if (recv_size == -1) { 
                 workers_finish++;
             } else {
-                vector<char> word_recvf(recv_size);
+                vector<char> word_recvf(recv_size);  
 
                 MPI_Recv(word_recvf.data(), recv_size, MPI_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     
                 string received_word(word_recvf.begin(), word_recvf.end());  
-
-                hash<string> hasher;
-                size_t hash_value = hasher(received_word);
-                size_t hash_value_m = 2 + (hash_value % (size - 2));
-                cout << received_word << " " << "hash value modulo: " << hash_value_m << endl;
-
-                int size_word = received_word.size();
-                MPI_Send(&size_word, 1, MPI_INT, hash_value_m, 0, MPI_COMM_WORLD);
-                MPI_Send(received_word.data(), received_word.size(), MPI_CHAR, hash_value_m, 0, MPI_COMM_WORLD);
+                cout << "Received word: " << received_word << endl;
             }
-        }
-
-        int stop_signal = -1;
-        for(int i = 2; i < size; i++) {
-            MPI_Send(&stop_signal, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-    }
-
-    if(rank > 1) {
-
-        while(true) {
-
-            int size_word;
-            MPI_Recv(&size_word, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            if(size_word == -1) {
-                break;
-            }
-            
-            vector<char> received_wordd(size_word);
-            MPI_Recv(received_wordd.data(), size_word, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            string word(received_wordd.begin(), received_wordd.end());
-
-            cout << rank << " recebeu a palavra " << word << endl;
         }
     }
 
